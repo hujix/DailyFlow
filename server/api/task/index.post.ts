87434980910134f4b9b1efcb2882631368eaf2cb
Hu.Sir <1668819658@qq.com>
2024-07-21 23:52:12 +0800

    🎨 优化task的新增接口参数

--- server/api/task/index.post.ts
+++ server/api/task/index.post.ts
@@ -2,20 +2,24 @@ import { serverSupabaseClient } from "#supabase/server";
 import type { Database } from "~/types/supabase.type";
 
 export default defineEventHandler(async (event) => {
-  const { tid, name, color } = (await readBody(event)) as { tid: string; name: string; color: string };
+  const { tid, name, color } = (await readBody(event)) as {
+    tid: string;
+    name: string;
+    color: string;
+  };
 
   const supabase = await serverSupabaseClient<Database>(event);
   const taskResult = await supabase.from("task").insert({
     type: "normal",
     tid,
     name,
-    color
-  }).select();
+    color,
+  });
 
   if (taskResult.error) {
     return {
       status: taskResult.error.code,
-      message: taskResult.error.message
+      message: taskResult.error.message,
     };
   }
 
